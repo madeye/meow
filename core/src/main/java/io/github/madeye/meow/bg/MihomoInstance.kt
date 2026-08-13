7e803948841e21c8bd77c0d885ed080dd67ef3c0
aimkiray <root@meowwoo.com>
2026-08-13 14:37:45 +0800

    Revert "feat(ipv6): wire Disable-IPv6 toggle to the engine resolver via FFI"
    This reverts commit 653cc5988dbdb84acb0935e0e28ea40b9edec769.

--- core/src/main/java/io/github/madeye/meow/bg/MihomoInstance.kt
+++ core/src/main/java/io/github/madeye/meow/bg/MihomoInstance.kt
@@ -97,7 +97,6 @@ class MihomoInstance(val profile: ClashProfile) {
 
         configFile.writeText(yaml)
         MihomoCore.nativeSetHomeDir(configDir.absolutePath)
-        MihomoCore.nativeSetIpv6Disabled(DataStore.disableIpv6)
         val result = MihomoCore.nativeStartEngine("127.0.0.1:9090", "")
         if (result != 0) {
             throw RuntimeException("Failed to start engine: ${MihomoCore.nativeGetLastError()}")
